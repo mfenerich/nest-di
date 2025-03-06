4a8f0931e30d4b9cc6c57500ae8d71a6c0e5de8a
mfenerich <marcelfenerich1@gmail.com>
2025-03-06 17:02:09 +0100

    Integrate PowerModule into CpuModule and enhance CpuService to utilize PowerService for power management

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CpuService } from './cpu.service';
+import { PowerModule } from '../power/power.module';
 
 @Module({
-  providers: [CpuService]
+  imports: [PowerModule], // This gives access to exported providers from PowerModule
+  providers: [CpuService],
+  exports: [CpuService], // Making CpuService available to other modules
 })
 export class CpuModule {}
