04ac054c3e466f7df721e1dd8031c87614ba64b2
mfenerich <marcelfenerich1@gmail.com>
2025-03-06 16:56:26 +0100

    Add PowerService export and supplyPower method to PowerModule

--- src/power/power.module.ts
+++ src/power/power.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { PowerService } from './power.service';
 
 @Module({
-  providers: [PowerService]
+  providers: [PowerService],
+  exports: [PowerService], // This makes PowerService available to other modules
 })
 export class PowerModule {}
